de4c6fceeed5b64866add6e919cb3cd70d7a0e52
Roy Yali <ryali93@gmail.com>
2025-04-11 12:27:05 +0200

    se agregaron covariables topograficas y se prueba a diferentes resoluciones

--- data_acquisition.py
+++ data_acquisition.py
@@ -9,6 +9,9 @@ import geopandas as gpd
 from geocube.api.core import make_geocube
 from geocube.rasterize import rasterize_points_griddata
 from config import *
+import rasterio as rio
+from rasterio.mask import mask
+
 
 # Inicializar los catálogos
 CATALOG_S3 = pystac_client.Client.open(S3_CATALOG_URL, modifier=planetary_computer.sign_inplace)
@@ -74,3 +77,26 @@ def get_s2(area_path, out_path, date_ini, date_end, row=0):
     ndvi = (clipped_s2[0][1] - clipped_s2[0][0]) / (clipped_s2[0][1] + clipped_s2[0][0])
     ndvi_reprojected = ndvi.rio.reproject(f"epsg:{EPSG_PROJECTED}")
     ndvi_reprojected.rio.to_raster(out_path, driver="GTiff")
+
+def get_dem(area_path, dem_path, out_path, row=0):
+    area = gpd.read_file(area_path).to_crs(epsg=EPSG_INPUT)
+    centroid = area.geometry.iloc[row].centroid
+    coords = [centroid.x, centroid.y]
+    
+    with rio.open(dem_path) as src:
+        # Ensure the area geometry is in the same CRS as the DEM
+        area = area.to_crs(src.crs)
+        dem_array, dem_transform = mask(src, [area.geometry.iloc[row]], crop=True)
+        dem_array = np.clip(dem_array, 0, None)
+        dem_array = dem_array[0]  # Extract the first band
+        dem_meta = src.meta.copy()
+    
+    dem_meta.update({
+        'height': dem_array.shape[0],
+        'width': dem_array.shape[1],
+        'transform': dem_transform,
+        'crs': src.crs.to_proj4(),
+    })
+    
+    with rio.open(out_path, 'w', **dem_meta) as dst:
+        dst.write(dem_array, 1)
